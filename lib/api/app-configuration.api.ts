78f2099bbc0bb8b3de20e2a8f9b46b4a0e502c49
Denis Briceag <dbriceag@griddynamics.com>
2026-03-23 16:40:49 +0200

    feat: project cleanup

--- lib/api/app-configuration.api.ts
+++ lib/api/app-configuration.api.ts
@@ -1,11 +1,12 @@
+import { cacheLife } from "next/cache"
+
 import { apiClient } from "@/lib/api/client"
 import { StoreConfig } from "@/types/store/store-config"
-import { cacheLife } from "next/cache"
 
 /**
  * @description We will cache app config at least for 1 week since it's not a dynamic data.
  * */
-export function getAppConfig() {
+export async function getAppConfig() {
   "use cache"
   cacheLife("weeks")
 
